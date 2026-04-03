662afb42f3a7f29967274f55a4bc0478a22da4ab
Daniel Duarte <danielduartemora27@gmail.com>
2026-04-03 21:51:02 +0200

    feat(ui): add navbar, links dropdown, and theme toggle

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { LinksDropdown } from "@/components/LinksDropdown";
+import { ThemeToggle } from "@/components/ThemeToogle";
+import { UserProfileDropdown } from "@/components/UserProfileDropdown";
+
+function Navbar() {
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between border-b border-border/40">
+      <div className="lg:hidden">
+        <LinksDropdown />
+      </div>
+
+      <div className="flex items-center gap-x-4 ml-auto lg:ml-0">
+        <ThemeToggle />
+        <UserProfileDropdown />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
