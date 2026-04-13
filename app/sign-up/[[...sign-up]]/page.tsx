f60125d5fb678dfc4ede785a63e028821781eb09
Daniel Duarte <danielduartemora27@gmail.com>
2026-04-13 16:47:39 +0200

    refactor(sign-up): use custom SignUpForm page layout

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import SignUpForm from "@/components/SignUpForm";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+
+export default async function SignUpPage() {
+  const { userId } = await auth();
+  const redirectPath = userId ? "/jobs" : "/";
+  return (
+    <main>
+      <header className="max-w-7xl mx-auto px-4 sm:px-8 py-2">
+        <Link href={redirectPath}>
+          <Image
+            src="/logo-career-flow.svg"
+            alt="career flow logo"
+            className="h-28 w-auto"
+            width={90}
+            height={40}
+            priority
+          />
+        </Link>
+      </header>
+      <section className="max-w-7xl mx-auto px-4 sm:px-8 py-8 lg:py-0 min-h-[calc(100vh-64px)] grid lg:grid-cols-2 items-center gap-8">
+        <Image
+          src="/main.svg"
+          alt="landing illustration"
+          className="hidden lg:block"
+          width={400}
+          height={400}
+        />
+        <div className="w-full max-w-lg ml-auto">
+          <SignUpForm />
+        </div>
+      </section>
+    </main>
+  );
+}
